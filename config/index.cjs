81f0f92a8dfb1ae3430855d33fb6bd38d2164360
ceerdekeee <131971204+ceerdekeee@users.noreply.github.com>
2023-04-27 20:21:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -28,11 +28,11 @@ const USER_CONFIG = {
       horoscopeDate: '08-07',
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '唯君', year: '1995', date: '08-07',
+         {
+          type: '生日', name: '涛', year: '1994', date: '04-19',
         },
         {
-          type: '生日', name: '涛', year: '1994', date: '04-19',
+          type: '生日', name: '唯君', year: '1995', date: '08-07',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '03-26',
@@ -41,7 +41,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-04-19' },
+        { keyword: 'love_day', date: '2023-04-01' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2023-08-07' },
       ],
